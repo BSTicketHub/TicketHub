554c8b2950d36eb3a69d96c0c06e00cbace35bb0
Unidentified-Lin <jp810412@gmail.com>
2020-09-03 00:21:43 +0800

    Add login page validation.

--- TicketHubApp/Controllers/AccountController.cs
+++ TicketHubApp/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using TicketHubApp.Models.ViewModels;
 
 namespace TicketHubApp.Controllers
 {
@@ -10,11 +11,42 @@ namespace TicketHubApp.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UserLogin(LoginViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View("Login");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ShopLogin(LoginViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View("Login");
+        }
+
         [HttpGet]
         public ActionResult LoginPlatform()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult LoginPlatform(LoginViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View("LoginPlatform");
+        }
     }
+
 }
---
+++ TicketHubApp/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketHubApp.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Account { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public bool IsSignUp { get; set; }
+    }
+}
