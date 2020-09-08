6aec17c04846117c8170ef6598189935b5125778
Unidentified-Lin <jp810412@gmail.com>
2020-09-08 11:19:52 +0800

    Implement user and shop login.

---
+++ TicketHubApp/Services/Models/UserToken.cs
@@ -0,0 +1,9 @@
+﻿namespace TicketHubApp.Services.Models
+{
+    public class UserToken
+    {
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+        public int PasswordWorkFactor { get; set; }
+    }
+}
