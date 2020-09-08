622d8dea3ecb47439474cb26c23a291597032f30
chenkai <zxc7788991@gmail.com>
2020-09-08 15:31:59 +0800

    change ticketslist css to scss, remove store_basic.js

--- TicketHubApp/Assets/Javascript/StoreBasic.js
+++
@@ -1,9 +0,0 @@
-﻿        // set sideBar title style
-        let sidebarTitle = document.querySelectorAll(".sidebar .div");
-        sidebarTitle.forEach(x => {x.classList.add("py-2", "pl-4")});
-        // set sideBar ul style
-        //let sidebarUl = document.querySelectorAll(".sidebar ul");
-        //sidebarUl.forEach(x => {x.classList.add("m-0", "p-0")});
-        // set sideBar li style
-        let sidebarLi = document.querySelectorAll(".sidebar li");
-        sidebarLi.forEach(x => {x.classList.add("py-2", "pl-4")});
