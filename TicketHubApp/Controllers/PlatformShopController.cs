7454fd8e3f19f643aebc9b5e2e57799e69da0625
Aus <abc2614856@gmail.com>
2020-09-08 19:57:18 +0800

    ShopList邏輯完成50%

--- TicketHubApp/Controllers/PlatformShopController.cs
+++ TicketHubApp/Controllers/PlatformShopController.cs
@@ -4,6 +4,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web.Mvc;
+using TicketHubApp.Models.ViewModels;
 using TicketHubApp.PlatformViewModels;
 using TicketHubDataLibrary.Models;
 
--- TicketHubApp/Models/ViewModels/ShopViewModel.cs
+++ TicketHubApp/Models/ViewModels/ShopViewModel.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using TicketHubDataLibrary.Models;
 
-namespace TicketHubApp.PlatformViewModels
+namespace TicketHubApp.Models.ViewModels
 {
     public class ShopViewModel
     {   
         public Guid Id { get; set; }
         public string ShopName { get; set; }
+        public string ShopIntro { get; set; }
         public string Phone { get; set; }
         public string Fax { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
         public string Address { get; set; }
         public string Email { get; set; }
         public string Website { get; set; }
+        public IEnumerable<SimpleIssueViewModel> Issues { get; set; }
     }
 }
