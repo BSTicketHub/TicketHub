0a10af40a15c445890bf3e6b0776a93f86d29630
Unidentified-Lin <jp810412@gmail.com>
2020-09-09 23:10:15 +0800

    Remove UserToken model.

--- TicketHubApp/Models/ServiceModels/UserToken.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TicketHubApp.Models.ServiceModels
-{
-    public class UserToken
-    {
-        public string PasswordHash { get; set; }
-        public string PasswordSalt { get; set; }
-        public int PasswordWorkFactor { get; set; }
-    }
-}
