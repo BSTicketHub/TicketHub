3daee66b4a5d8b5363885e9bf13fa08def9e23f0
Unidentified-Lin <jp810412@gmail.com>
2020-09-10 20:00:34 +0800

    Seed users.

--- TicketHubDataLibrary/Migrations/Configuration.cs
+++ TicketHubDataLibrary/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
 using System.Data.Entity.Migrations;
+using System.Linq;
 using TicketHubDataLibrary.Models;
 
 namespace TicketHubDataLibrary.Migrations
@@ -20,6 +23,36 @@ namespace TicketHubDataLibrary.Migrations
                 new IdentityRole { Id = "4", Name = RoleName.SHOP_EMPLOYEE },
                 new IdentityRole { Id = "5", Name = RoleName.CUSTOMER }
             );
+
+            string[] userNames = new string[] { "admin", "platform", "manager", "employee", "cust" };
+            GenUsers(context, userNames);
+
+        }
+
+        private void GenUsers(TicketHubContext context, string[] userNames)
+        {
+            var userStore = new UserStore<TicketHubUser>(context);
+            var userManager = new UserManager<TicketHubUser>(userStore);
+
+            for (int i = 1; i <= 5; i++)
+            {
+                var name = userNames[i - 1];
+                var roleName = context.Roles.FirstOrDefault(r => r.Id == i.ToString()).Name;
+                for (int j = 1; j <= i * i; j++)
+                {
+                    GenUser(context, userManager, $"{name}{j}", roleName);
+                }
+            }
+        }
+
+        private void GenUser(TicketHubContext context, UserManager<TicketHubUser> userManager, string userName, string roleName)
+        {
+            if (!(context.Users.Any(u => u.UserName == userName)))
+            {
+                var userToInsert = new TicketHubUser { UserName = userName, PhoneNumber = "0987654321", Email = $"{userName}@tickethub.com" };
+                userManager.Create(userToInsert, "Pwd12345.");
+                userManager.AddToRole(userToInsert.Id, roleName);
+            }
         }
     }
 }
