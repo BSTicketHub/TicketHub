599e31f3d50cdce108173d69c28fd89f030c0630
Unidentified-Lin <jp810412@gmail.com>
2020-09-10 14:28:52 +0800

    Alter Ticket table columns to nullable.

---
+++ TicketHubDataLibrary/Migrations/202009100626415_TicketExchangeNullable.cs
@@ -0,0 +1,24 @@
+﻿namespace TicketHubDataLibrary.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TicketExchangeNullable : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Tickets", "Exchanged", c => c.Boolean());
+            AlterColumn("dbo.Tickets", "ExchangedDate", c => c.DateTime());
+            AlterColumn("dbo.Tickets", "Voided", c => c.Boolean());
+            AlterColumn("dbo.Tickets", "VoidedDate", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Tickets", "VoidedDate", c => c.DateTime(nullable: false));
+            AlterColumn("dbo.Tickets", "Voided", c => c.Boolean(nullable: false));
+            AlterColumn("dbo.Tickets", "ExchangedDate", c => c.DateTime(nullable: false));
+            AlterColumn("dbo.Tickets", "Exchanged", c => c.Boolean(nullable: false));
+        }
+    }
+}
