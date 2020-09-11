1132a465862ffd17ca1586a25ae92e957ce567d3
Unidentified-Lin <jp810412@gmail.com>
2020-09-11 16:14:44 +0800

    Fix Seed user and role(Create/Update).

--- TicketHubDataLibrary/Migrations/Configuration.cs
+++ TicketHubDataLibrary/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity.Migrations;
 using System.Linq;
@@ -33,25 +34,36 @@ namespace TicketHubDataLibrary.Migrations
         {
             var userStore = new UserStore<TicketHubUser>(context);
             var userManager = new UserManager<TicketHubUser>(userStore);
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
 
             for (int i = 1; i <= 5; i++)
             {
                 var name = userNames[i - 1];
-                var roleName = context.Roles.FirstOrDefault(r => r.Id == i.ToString()).Name;
+                var roleName = roleManager.FindById(i.ToString()).Name;
                 for (int j = 1; j <= i * i; j++)
                 {
-                    GenUser(context, userManager, $"{name}{j}", roleName);
+                    GenUser(userManager, $"{name}{j}", roleName);
                 }
             }
         }
 
-        private void GenUser(TicketHubContext context, UserManager<TicketHubUser> userManager, string userName, string roleName)
+        private void GenUser(UserManager<TicketHubUser> userManager, string userName, string roleName)
         {
-            if (!(context.Users.Any(u => u.UserName == userName)))
+            var userEmail = $"{userName}@tickethub.com";
+            if (!userManager.Users.Any(u => u.UserName == userEmail))
             {
-                var userToInsert = new TicketHubUser { UserName = $"{userName}@tickethub.com", PhoneNumber = "0987654321", Email = $"{userName}@tickethub.com" };
+                var userToInsert = new TicketHubUser { UserName = userEmail, PhoneNumber = "0987654321", Email = userEmail };
                 userManager.Create(userToInsert, "Pwd12345.");
                 userManager.AddToRole(userToInsert.Id, roleName);
+            }
+            else
+            {
+                var user = userManager.FindByEmail($"{userName}@tickethub.com");
+                if (!userManager.IsInRole(user.Id, roleName))
+                {
+                    userManager.AddToRole(user.Id, roleName);
+                }
             }
         }
     }
