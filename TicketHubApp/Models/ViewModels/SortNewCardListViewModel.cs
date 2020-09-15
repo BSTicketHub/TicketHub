56a98f9dd5d669e1d38c49798ae14c71e4950234
luliya <ykk40627@gmail.com>
2020-09-15 16:31:30 +0800

    #抓資料庫資料

---
+++ TicketHubApp/Models/ViewModels/SortNewCardListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicketHubApp.Models.ViewModels
+{
+    public class SortNewCardListViewModel
+    {
+        public List<SortNewCardViewModel> Items { get; set; }
+            
+     }
+}
