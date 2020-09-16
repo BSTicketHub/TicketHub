76730d4697bf7f5c9cf28144ea79e9e50b7f4230
luliya <ykk40627@gmail.com>
2020-09-16 11:40:58 +0800

    #首頁Card抓資料庫資料完成，建立了很多ViewModel→"BestSellerCardViewModel"、"RecommenCardViewModel"、"SortNewCardViewModel"，購物車先建立Model

---
+++ TicketHubApp/Models/ViewModels/HomeListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicketHubApp.Models.ViewModels
+{
+    public class HomeListViewModel
+    {
+
+        public BestSellerCardListViewModel BestSellerItems { get; set; }
+        public RecommenCardListViewModel RecommenItems { get; set; }
+        public SortNewCardListViewModel SortNewItems { get; set; }
+    }
+}
