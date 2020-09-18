27a7896abc68870adb33a737007c4e354569703e
luliya <ykk40627@gmail.com>
2020-09-18 18:35:12 +0800

    #修改ViewModel

--- TicketHubApp/Models/ViewModels/CartViewModel.cs
+++ TicketHubApp/Models/ViewModels/CartViewModel.cs
@@ -5,7 +5,32 @@ using System.Web;
 
 namespace TicketHubApp.Models.ViewModels
 {
+    [Serializable] //可序列化
     public class CartViewModel
     {
+        //建構值
+        public CartViewModel()
+        {
+            this.cartItems = new List<CartItemViewModel>();
+        }
+        
+        //儲存所有商品
+        public List<CartItemViewModel> cartItems;
+
+        //取得商品總價
+        public decimal TotalAmount
+        {
+            get
+            {
+                decimal totalAmount = 0.0m;
+                foreach (var cartItem in this.cartItems)
+                {
+                    totalAmount = totalAmount + cartItem.Amount;
+                }
+                return totalAmount;
+            }
+        }
     }
+
+    
 }
