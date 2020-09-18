5907a5cf87ad6002e1aab08b705597a8375da12a
Aus <abc2614856@gmail.com>
2020-09-19 03:38:45 +0800

    訂單、持有票券資料串接完成

---
+++ TicketHubApp/Models/ViewModels/CustomerOrderViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TicketHubDataLibrary.Models;
+
+namespace TicketHubApp.Models.ViewModels
+{
+    public class CustomerOrderViewModel
+    {
+        public Guid Id { get; set; }
+        public string OrderDate { get; set; }
+        public string UserId { get; set; }
+        public IEnumerable<ShopIssueViewModel> OrderIssue { get; set; }
+    }
+}
