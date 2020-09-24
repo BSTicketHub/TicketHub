9bd4ec78fc30f08607e27ec6a35340c80af8d687
Shane <sushifantasy@hotmail.com>
2020-09-24 22:15:47 +0800

    修改productCart抓取資料方式，修改ProductCart CSS 及html小部分修改 原JS撰寫，改template顯示

---
+++ TicketHubApp/Models/ViewModels/ProductCartViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TicketHubDataLibrary.Models;
+
+namespace TicketHubApp.Models.ViewModels
+{
+    public class ProductCartViewModel
+    {
+        public Guid Id { get; set; }
+        public DateTime OrderedDate { get; set; }
+        public Guid OrderId { get; set; }
+        public Order Order { get; set; }
+        public Guid IssueId { get; set; }
+        public Issue Issue { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Price { get; set; }
+        public string Title { get; set; }
+        public string Intro { get; set; }
+        public string Memo { get; set; }
+        public string ImgPath { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public decimal DiscountPrice { get; set; }
+
+    }
+}
