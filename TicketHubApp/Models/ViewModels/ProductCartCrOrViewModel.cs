8ecf5e4fabe86d094f2b7a2ca4826c2091fbc095
Shane <sushifantasy@hotmail.com>
2020-10-02 23:24:35 +0800

    新增結帳按鍵功能，創order資料庫資料

---
+++ TicketHubApp/Models/ViewModels/ProductCartCrOrViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicketHubApp.Models.ViewModels
+{
+    public class ProductCartCrOrViewModel
+    {
+        public string UserId { get; set; }
+        public List<Guid> id { get; set; }
+        public List<int> amount { get; set; }
+    }
+}
